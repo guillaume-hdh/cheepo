c8bc934dfa2f7c01bf98ccf78e93c74f776e0fd5
guillaume-hdh <contact@3w-hexagone.fr>
2025-08-25 22:53:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT : assets en chemins relatifs pour servir depuis /public_html/app
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
